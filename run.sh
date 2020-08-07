e7a82ea02d2d587df05a248ab46350ceaa8a73bc
Carter Turn <carterturn@tutanota.de>
2020-08-07 14:12:23 -0700

    Fake secrets of OSX/Linux. Finding bash better

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if [ ! -f sps_web_2020/secrets.py ]; then
+	venv/bin/python3 fake_secrets.py
+fi
 
 venv/bin/python3 manage.py runserver
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 python -m pip install virtualenv
 
